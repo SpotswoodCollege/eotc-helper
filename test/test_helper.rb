8e833868a17fd89a9df0afae9e754560307518cf
Microlith57 <microlith57@gmail.com>
2018-05-24 19:55:26 +1200

    Move Coveralls up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-
 require 'coveralls'
 Coveralls.wear!
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
