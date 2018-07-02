10ea63e957a336f8aebe95865efa0b4cdb26b911
microlith57 <microlith57@gmail.com>
2018-07-03 08:51:08 +1200

    Add documentation to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
+# This controller describes the behaviour of Users, and is accessed through the /users route.
 class UsersController < ApplicationController
+  # Show the page of a given User
+  # TODO: Remove this method; replace with a /settings page
   def show
     @user = User.find(params[:id])
   end
-
+  
+  # Make a new User
   def new
     @user = User.new
   end
-
+  
+  # Create a User
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,6 +23,7 @@ class UsersController < ApplicationController
 
   private
 
+  # Get the parameters of a User
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
