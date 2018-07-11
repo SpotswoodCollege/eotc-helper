c9cf2a654ae20801bd0c11150dd76caeb1bde1db
Microlith57 <microlith57@gmail.com>
2018-07-12 08:44:03 +1200

    Document SessionsController and SessionsHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
+# The SessionsController class controls sessions. A session is a logged-in
+# client.
 class SessionsController < ApplicationController
   def new; end
 
+  # Create a Session object
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
-  # Log in the given user
+  # Log in the given User
+  # Does not return a value
   def log_in(user)
     session[:user_id] = user.id
   end
