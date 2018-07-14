2f1c2056057d579dd69c62603aaa2ccd20d28946
microlith57 <microlith57@gmail.com>
2018-07-14 21:39:53 +1200

    style(lint): Fix linting issues
    Fixes 7 offenses - 6 trailing whitespace; 1 line too long
    
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # TODO: This was from https://www.railstutorial.org/
   #       and a better one might exist.
-  
+
   # Emails are lowercase
   before_save { self.email = email.downcase }
-  
+
   # Name must be present and up to 50 characters
   validates :name,  presence: true,
                     length: { maximum: 50 }
-  
+
   # Email must be present, unique, a valid email, and up to 255 characters
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  
+
   # Users have secure passwords
   has_secure_password
 
