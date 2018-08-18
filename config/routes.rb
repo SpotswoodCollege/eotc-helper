a4de4d5988616d673c67175f3d6735761dfaa0cd
microlith57 <microlith57@gmail.com>
2018-08-18 12:22:49 +1200

    revert(users): Revert all user-based changes, removing users entirely
    We're switching to devise/cancancan!
    
    re #37
    
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
-  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
