384206d55c9f2ccf4d7eb8e2189c37f0e04473a7
microlith57 <microlith57@gmail.com>
2018-12-06 12:00:53 +1300

    test(groups): Add a test for groups
    This test ensures that indexing groups succeeds and results in a link to Science 101 (using a fixture).
    
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get groups_index_url
+    get groups_url
     assert_response :success
+    assert_select 'a', 'Science 101', 'There must be a Science 101 link'
   end
 end
