07f629c99a2d334be6d434687f49849dcc448813
microlith57 <microlith57@gmail.com>
2018-12-06 11:55:42 +1300

    fix(groups): Fix issue where subscriptions could have invalid groups
    Now uses validations.
    
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,7 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :user,  presence: { message: 'must belong to a user' }
+  validates :group, presence: { message: 'must belong to a group' }
 end
