421a6c540570edc8e8ca1a46850bb29ee518124c
microlith57 <microlith57@gmail.com>
2018-12-05 17:20:04 +1300

    fix(groups): Make the validation message for creator more explanatory
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   validates :name, presence: { message: "can't be blank" },
                    uniqueness: { message: 'must be unique' }
 
-  validates :creator, presence: { message: "can't be blank" }
+  validates :creator, presence: { message: 'must be logged in' }
 
   has_many :subscriptions, dependent: :destroy
   has_many :users, through: :subscriptions
