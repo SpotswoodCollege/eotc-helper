ad8c75e9945fd0e7b7e9c823e5caacf2fc883785
microlith57 <microlith57@gmail.com>
2018-12-06 14:14:31 +1300

    test(groups): Add more groups tests This adds the following tests: - Ensure standard users cannot create groups - Ensure teacher users can - Ensure groups cannot be created with blank names - Ensure blank descriptions are fine
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,9 +1,37 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'should get index' do
     get groups_url
     assert_response :success
     assert_select 'a', 'Science 101', 'There must be a Science 101 link'
+  end
+
+  test 'should not create group with standard role' do
+    sign_in users(:average_joe)
+    assert_raises 'RuntimeError: Must be logged in' do
+      group = Group.new name: 'PEH101', description: 'Physical Education Level 1'
+      group.save!
+    end
+  end
+
+  test 'should be able to create group as teacher' do
+    sign_in users(:teacher_kate)
+    group = Group.new name: 'PEH101', description: 'Physical Education Level 1'
+    assert group.save
+  end
+
+  test 'should not be able to create group with blank name' do
+    sign_in users(:teacher_kate)
+    group = Group.new description: 'Physical Education Level 1'
+    assert_not group.save
+  end
+
+  test 'should be able to create group with blank description' do
+    sign_in users(:teacher_kate)
+    group = Group.new name: 'PEH101'
+    assert group.save
   end
 end
