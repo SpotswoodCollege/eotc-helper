1012de77b4b7f1cb0db099776d75ebe8cfe25537
microlith57 <microlith57@gmail.com>
2018-12-09 12:46:53 +1300

    feat(activities): Add `new` and `create` actions Complete with a form (in dire need of more work)
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,28 @@ class ActivitiesController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
+    @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+
+    @activity.creator = current_user.id if user_signed_in?
+
+    if @activity.save
+      redirect_to @activity
+    else
+      render 'new', status: :bad_request
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :activity_type, :risk)
   end
 end
