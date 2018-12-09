797bd35a095f391e0f5d419f8d6813f4fa4aaacb
microlith57 <microlith57@gmail.com>
2018-12-09 14:33:36 +1300

    feat(activities): Add hash of activity guidelines Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,55 @@
 class ActivitiesController < ApplicationController
+  # Official Ministry of Education guidelines from the EOTC Guidelines 2016
+  # Source: http://eotc.tki.org.nz/EOTC-home/EOTC-Guidelines
+  # Accessed Friday December 7, 2018
+  # TODO: Should this be in the model? Or possibly a config file?
+  ACTIVITY_DATA = {
+    in_school: {
+      low_risk: {
+        approval: :none,
+        parental_consent: :none,
+        risk_management_planning: :usual_lesson_planning
+      }, high_risk: {
+        approval: :blanket,
+        parental_consent: :blanket,
+        risk_management_planning: %i[usual_lesson_planning generic]
+      }
+    },
+    community: {
+      low_risk: {
+        approval: %i[senior coordinator],
+        parental_consent: %i[none blanket],
+        risk_management_planning: :generic
+      }, high_risk: {
+        approval: %i[senior coordinator],
+        parental_consent: %i[blanket separate],
+        risk_management_planning: :generic
+      }
+    },
+    day_trip:  {
+      low_risk: {
+        approval: %i[senior coordinator],
+        parental_consent: %i[none blanket],
+        risk_management_planning: :generic
+      }, high_risk: {
+        approval: %i[principal coordinator],
+        parental_consent: :separate_and_risk_disclosure,
+        risk_management_planning: :specific
+      }
+    },
+    multi_day: {
+      low_risk: {
+        approval: %i[principal coordinator],
+        parental_consent: :separate,
+        risk_management_planning: :specific
+      }, high_risk: {
+        approval: %i[principal board],
+        parental_consent: :separate_and_risk_disclosure,
+        risk_management_planning: :specific
+      }
+    }
+  }.freeze
+
   def index
     @activities = Activity.all
   end
