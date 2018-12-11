46a9aa91e77b42c74966c0a222d00033f741e028
microlith57 <microlith57@gmail.com>
2018-12-12 11:38:06 +1300

    feat(activities): Add approval setup Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -81,18 +81,55 @@ class ActivitiesController < ApplicationController
 
   def update
     @activity = Activity.find(params[:id])
-    @activity.edited_at = @activity.updated_at
 
-    if @activity.update(activity_params)
-      redirect_to @activity
+    if activity_params[:approve] == true
+      # Approve activity
+      @activity.approved_at = Time.now if activity_params[:approve] == true &&
+                                          can?(:approve, @activity)
+      if @activity.update
+        redirect_to @activity
+      else
+        render 'edit', status: :bad_request
+      end
+    elsif would_change(activity_params_no_approve, @activity)
+      # Edit activity
+      @activity.edited_at = Time.now
+
+      if @activity.update(activity_params_no_approve)
+        redirect_to @activity
+      else
+        render 'edit', status: :bad_request
+      end
     else
-      render 'edit', status: :bad_request
+      redirect_to @activity || activities_path
     end
   end
 
   private
 
   def activity_params
-    params.require(:activity).permit(:name, :description, :activity_type, :risk)
+    params.require(:activity).permit(:name,
+                                     :description,
+                                     :activity_type,
+                                     :risk,
+                                     :approve)
+  end
+
+  def activity_params_no_approve
+    params.require(:activity).permit(:name,
+                                     :description,
+                                     :activity_type,
+                                     :risk)
+  end
+
+  def would_change(some_params, activity)
+    would_change_param(some_params[:name], activity.name) ||
+      would_change_param(some_params[:description], activity.description) ||
+      would_change_param(some_params[:activity_type], activity.activity_type) ||
+      would_change_param(some_params[:risk], activity.risk)
+  end
+
+  def would_change_param(param, activity_param)
+    !param.nil? && param != activity_param
   end
 end
