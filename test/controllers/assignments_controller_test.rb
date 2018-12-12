93ba35bf3210f66cdcf13fe2651fa9f370d99985
microlith57 <microlith57@gmail.com>
2018-12-12 14:24:30 +1300

    test(assignments): Remove autogenerated tests Signed-off-by: microlith57 <microlith57@gmail.com>

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -1,19 +1,8 @@
 require 'test_helper'
 
 class AssignmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get assignments_create_url
+  test 'should get index' do
+    get assignments_url
     assert_response :success
   end
-
-  test "should get destroy" do
-    get assignments_destroy_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get assignments_index_url
-    assert_response :success
-  end
-
 end
