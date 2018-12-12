85e8acd58e3d9d6b577ccfd9a1e6c65204e7f6d1
microlith57 <microlith57@gmail.com>
2018-12-12 14:24:48 +1300

    style(quotes): Use single quotes Signed-off-by: microlith57 <microlith57@gmail.com>

--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ActivitiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get activities_url
     assert_response :success
   end
