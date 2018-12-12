18cc7e8b98fbdf1af4a2c2d535b273534f05a748
microlith57 <microlith57@gmail.com>
2018-12-12 14:22:24 +1300

    fix(associations): Validate uniqueness as well Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,6 @@
 class Assignment < ApplicationRecord
   belongs_to :activity
   belongs_to :group
+
+  validates :group_id, uniqueness: { scope: :activity_id }
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,4 +6,6 @@ class Subscription < ApplicationRecord
     I18n.t('error.brief.belong_to', rel: 'user') }
   validates :group, presence: { message:
     I18n.t('error.brief.belong_to', rel: 'user') }
+
+  validates :group_id, uniqueness: { scope: :user_id }
 end
