f9dd1862262b545e7f29a60e8eef8f38266b27da
microlith57 <microlith57@gmail.com>
2018-12-15 10:37:33 +1300

    style(activities): Use correct hash syntax Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   load_and_authorize_resource
-  skip_authorize_resource :only => :update
+  skip_authorize_resource only: :update
 
   def index
     @activities = Activity.all
