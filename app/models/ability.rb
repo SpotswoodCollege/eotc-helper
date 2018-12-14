18e9538acc3861a0bb90cd89a9b13ae762861998
microlith57 <microlith57@gmail.com>
2018-12-14 15:50:50 +1300

    fix(abilities): Move into separate methods Also start on the Great Rework (tm)
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -29,57 +29,65 @@ class Ability
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
-    #---# GENERAL
-
     alias_action :create, :read, :update, :destroy, to: :crud
 
     # Guest user
     user ||= User.new
 
     # Administrators can do everything
-    can :manage, :all if user.of_group? :administrators
+    if user.role? :administrator
+      can :manage, :all
+      return
+    end
 
-    #---# GROUPS
+    init_standard user
+    init_staff    user if user.of_group? :staff
+  end
 
-    # Anyone can see groups
-    can :read, Group
+  private
 
-    # Teachers can create groups
-    can :create, Group if user.of_group? :teachers
-
-    # Teachers can manage their groups
-    can :manage, Group, creator: user.id if user.of_group? :teachers
-
-    # Coordinators can manage all groups
-    can :manage, Group if user.role_gte? :coordinator
-
-    #---# SUBSCRIPTIONS
-
-    # Can subscribe
-    can :create, Subscription if user.present?
-
-    # Can read / destroy own subscriptions
-    can %i[read destroy], Subscription, user_id: user.id if user.present?
-
-    #---# ACTIVITIES
-
-    # Anyone can see approved activities
-    can :read, Activity, &:approved?
-
-    # Teachers can see all activities, and create more
-    can %i[read create], Activity if user.of_group? :teachers
-
-    # Teachers can manage their activities
-    can :crud, Activity, creator: user.id if user.of_group? :teachers
-
-    # Coordinators can manage all activities
-    can :crud, Activity if user.role_gte? :coordinator
-
-    # Only some users can approve activities
-    # Decided by the Activity model
-    cannot :approve, Activity
-    can :approve, Activity do |activity|
+  # Standard privileges applied to all users
+  def init_standard(user)
+    # Can read approved activities; can manage own activities; delegate approval
+    #   to Activity model
+    can :read,                  Activity, &:approved?
+    can %i[read update delete], Activity, creator: user.id
+    cannot :approve,            Activity
+    can :approve,               Activity do |activity|
       activity.can_be_approved_by? user
     end
+
+    # Can read all groups; can manage own groups
+    can :read,                  Group
+    can %i[read update delete], Group, creator: user.id
+
+    # Can create subscriptions; can see/delete own subscriptions
+    can :create,         Subscription                   if user.present?
+    can %i[read delete], Subscription, user_id: user.id if user.present?
+
+    # Can read assignments when their activities are also visible
+    cannot :read, Assignment
+    can    :read, Assignment do |assignment|
+      can? :read, assignment.activity
+    end
   end
+
+  def init_staff(_user)
+    # Can read & create activities
+    can %i[read create], Activity
+
+    # Can create groups
+    can :create, Group
+
+    # Can create assignments; can delete assignments where activity/group is
+    #   also deletable
+    can :create,    Assignment
+    cannot :delete, Assignment
+    can :delete,    Assignment do |assignment|
+      can?(:delete, assignment.group) || can?(:delete, assignment.activity)
+    end
+  end
+
+  # TODO: Add all important roles etc.
+  #       Awaiting @spottyboy
 end
