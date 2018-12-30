cb6c289b9afabaac5515f7abe2a377d24731eb88
microlith57 <microlith57@gmail.com>
2018-12-31 11:59:04 +1300

    test(users): Sign in before attemting to access preferences
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- test/controllers/preferences_controller_test.rb
+++ test/controllers/preferences_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PreferencesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  include Rails.application.routes.url_helpers
+
   test 'should get show' do
+    sign_in users(:average_joe)
     get preferences_url
     assert_response :success
   end
